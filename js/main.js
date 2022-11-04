5fd549e82377532d9ed1d5d18ce0cc315e625e54
nodoor999 <hmq1986@gmail.com>
2022-11-05 01:03:41 +0900

    ff

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	/*
+	
 	// Header Scroll
 	$(window).on('scroll', function() {
 		var scroll = $(window).scrollTop();
@@ -10,7 +10,7 @@ $(document).ready(function() {
 			$('#header').removeClass('fixed');
 		}
 	});
-*/
+
 	// Fancybox
 	$('.work-box').fancybox();
 
